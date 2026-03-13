741a35ed953550f0f4a13b4fe9588ff024a96aca
lganic <55366941+lganic@users.noreply.github.com>
2026-03-13 19:38:04 -0400

    Added sound effects to dispensers

--- scripts/food_dispenser.gd
+++ scripts/food_dispenser.gd
@@ -1,11 +1,13 @@
 extends Area2D
 
 @export var scene_to_spawn: PackedScene
+@onready var audio_player: AudioStreamPlayer2D = $AudioStreamPlayer2D
 
 func _input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_RIGHT and event.pressed:
 			spawn_scene()
+			audio_player.play()
 
 
 func spawn_scene():
